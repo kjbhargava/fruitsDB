636bed794aa350697752eb0823b7bbbab108454d
Kjbhargava <karanjbhargava@gmail.com>
2019-10-27 19:44:54 -0700

    adding documents to the mongoose schema

--- app.js
+++ app.js
@@ -2,12 +2,26 @@
 
 const mongoose = require('mongoose');
 
+//need a connection URL
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
+//after creating the mongoDB database, we need to create a new schema
 const fruitSchema = new mongoose.Schema ({
     name: String,
     rating: Number,
     review: String
 });
 
-const Fruit = mongoose.model("")
+//after creating the schema, we need to makea  Mongoose model
+//ask yourself what is the name of the collection for this schema?
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+//create a new document based on the "Fruit" model above
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit"
+});
+
+//fruit.save saves the fruit document inside the fruit collection inside the fruits DB
+fruit.save();
